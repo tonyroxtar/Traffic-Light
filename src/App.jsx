7da594f4e0a5d6c0f0b8024500c365a69b9a9431
tonyroxtar <antonio.chamal@gmail.com>
2024-03-04 21:01:08 -0300

    first commit (se vienen cositas)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import { TrafficLight } from './components/traffic-light'
+
+function App() {
+  
+  return (
+    <>
+    <TrafficLight/>
+    </>
+  )
+}
+
+export default App
